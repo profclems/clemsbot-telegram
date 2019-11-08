9efb49377c717fd37df5c5d9171e7df5aac229c9
Clement Sam <clementsam75@gmail.com>
2019-11-08 01:46:37 +0000

    new

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+//https://api.telegram.org/bot697407449:AAEgFaWujPurIK21oMZ1RyR66IjJbDnptMk/setWebhook?url=clemsbot.herokuapp.com
+require 'vendor/autoload.php';
+
+$client = new Zelenin\Telegram\Bot\Api('697407449:AAEgFaWujPurIK21oMZ1RyR66IjJbDnptMk'); // Set your access token
+$url = ''; // URL RSS feed
+$update = json_decode(file_get_contents('php://input'));
+
+//your app
+try {
+
+    if($update->message->text == '/email')
+    {
+    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+    	$response = $client->sendMessage([
+        	'chat_id' => $update->message->chat->id,
+        	'text' => "You can send email to : clementsam75@gmail.com"
+     	]);
+    }
+    else if($update->message->text == '/help')
+    {
+    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+    	$response = $client->sendMessage([
+    		'chat_id' => $update->message->chat->id,
+    		'text' => "List of commands :\n /email -> Get email address of the owner \n /latest -> Get latest posts of the blog 
+    		/help -> Shows list of available commands"
+    		]);
+
+    }
+    else if($update->message->text == '/latest')
+    {
+    		Feed::$cacheDir 	= __DIR__ . '/cache';
+			Feed::$cacheExpire 	= '5 hours';
+			$rss 		= Feed::loadRss($url);
+			$items 		= $rss->item;
+			$lastitem 	= $items[0];
+			$lastlink 	= $lastitem->link;
+			$lasttitle 	= $lastitem->title;
+			$message = $lasttitle . " \n ". $lastlink;
+			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+			$response = $client->sendMessage([
+					'chat_id' => $update->message->chat->id,
+					'text' => $message
+				]);
+
+    }
+    else
+    {
+    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+    	$response = $client->sendMessage([
+    		'chat_id' => $update->message->chat->id,
+    		'text' => "Invalid command, please use /help to get list of available commands"
+    		]);
+    }
+
+} catch (\Zelenin\Telegram\Bot\NotOkException $e) {
+
+    //echo error message ot log it
+    //echo $e->getMessage();
+
+}
